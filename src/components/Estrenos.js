9335f5b82e22f8b0c1836e0c033cfa3df6d2460e
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2018-04-02 15:46:42 -0300

    Estilos
    - Aplicación de estilos con styled-components para crear un nuevo
    componente ItemPelicula.

--- src/components/Estrenos.js
+++ src/components/Estrenos.js
@@ -3,7 +3,47 @@ import styled from 'styled-components';
 
 const Contenedor = styled.div`
     >ul{
-        color:green;
+        display:flex;
+        overflow:scroll;
+        padding-left:0px;
+        background:rgba(25,25,200,1);
+    }
+`;
+
+const ItemPelicula = styled.li`
+    list-style:none;
+    margin:5px;
+    min-width:200px; 
+    height:300px;
+    background-size: cover;
+    color:white;
+    font-family: 'Chicle', cursive;
+    text-align:center;
+    font-size:25px;
+    position:relative;
+    >span{
+        background:rgba(12,12,12,0.8);
+        padding: 0 10px;
+        display:none;
+    }
+    >img{
+        position:absolute;
+        width:100%;
+        height:100%;
+        top:0;
+        left:0;
+        object-fit:cover;
+    }
+
+    &:hover{
+        background:rgba(25,25,25,0.5);
+        >span{
+            display:block;
+            position:relative;
+        }
+        >img{
+            opacity:0.5;
+        }
     }
 `;
 
@@ -13,9 +53,12 @@ export default (props) => {
             Estrenos
             <ul>
                 {
-                    props.data.map( (pelicula)=>(
-                        <li key={pelicula.id}>{pelicula.title}</li>
-                    ) )
+                    props.data.map((pelicula) => (
+                        <ItemPelicula key={pelicula.id} img={pelicula.poster_path}>
+                            <img src={"https://image.tmdb.org/t/p/w500"+pelicula.poster_path} alt=""/>
+                            <span>{pelicula.title}</span>
+                        </ItemPelicula>
+                    ))
                 }
             </ul>
         </Contenedor>
